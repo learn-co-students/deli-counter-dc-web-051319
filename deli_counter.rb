8768621d220aac77f1cbd355bf025335ed9d5cc8
Matt Williams <matt.seongjun@gmail.com>
2019-03-29 14:14:10 -0400

    Ruby one-liners are fun

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def line(line, string = "")
+    if string == "" && line.length < 1 
+        return puts "The line is currently empty."
+    end
+    
+    if line.length < 1 
+        return puts "The line is currently:#{string}"
+    else 
+        string = " #{line.length}. #{line[line.length - 1]}" + string
+        line.pop
+        line(line, string) 
+    end
+end
+
+def take_a_number(line, name)
+    line << name
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+    puts !(line) || line.length < 1 ? "There is nobody waiting to be served!" : "Currently serving #{line.shift}."
+end
+
